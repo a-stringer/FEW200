6497234f56035d003347fb58272ef92886b4cb06
a-stringer <abstringer82@gmail.com>
2019-05-30 18:25:08 +0000

    Day 2 early afternoon

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,28 +1,42 @@
 import { TodoListItem } from './models';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { TodoDataService } from './todo-data.service';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class ToDoListComponent implements OnInit {
-  items: TodoListItem[];
+export class ToDoListComponent implements OnInit, OnDestroy {
+  items$: Observable<TodoListItem[]>;
+  // items: TodoListItem[];
+  // subscription: Subscription;
 
   constructor(private service: TodoDataService) { }
 
   ngOnInit() {
-    this.items = this.service.getTodoList();
+    this.items$ = this.service.getTodoList();
+
+    //   this.subscription = this.items$.subscribe(tdl => {
+    //     console.log('Got a new todo list', tdl);
+    //     this.items = tdl;
+    //   });
   }
+
+  ngOnDestroy() {
+    // this.subscription.unsubscribe();
+  }
+
   markComplete(item: TodoListItem) {
-    item.completed = true;
+    // item.completed = true;
   }
   add(what: string) {
-    this.items.unshift({ description: what, completed: false });
+    // this.items.unshift({ description: what, completed: false });
+    this.service.add(what);
   }
   removeCompleted() {
-    this.items = this.items.filter(item => item.completed === false);
+    // this.items = this.items.filter(item => item.completed === false);
   }
 }
 
