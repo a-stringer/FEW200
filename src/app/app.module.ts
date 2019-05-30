544c454afd9253817e329ea551530a571776317b
a-stringer <abstringer82@gmail.com>
2019-05-30 17:14:07 +0000

    Pre Lunch, day 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { ToDoListComponent } from './components/todo-list/todo-list.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { TipCalculatorComponent } from './components/tip-calculator/tip-calculator.component';
 import { NavComponent } from './components/nav/nav.component';
+import { EntryComponent } from './components/todo-list/entry/entry.component';
+import { ListComponent } from './components/todo-list/list/list.component';
+import { TodoDataService } from './components/todo-list/todo-data.service';
 
 @NgModule({
   declarations: [
@@ -14,13 +17,15 @@ import { NavComponent } from './components/nav/nav.component';
     ToDoListComponent,
     DashboardComponent,
     TipCalculatorComponent,
-    NavComponent
+    NavComponent,
+    EntryComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [TodoDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,27 +1,28 @@
-import { Component } from '@angular/core';
 import { TodoListItem } from './models';
+import { Component, OnInit } from '@angular/core';
+import { TodoDataService } from './todo-data.service';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class ToDoListComponent {
-  items: TodoListItem[] = [
-    { description: 'Fix Front Door', completed: false },
-    { description: 'Change bathroom lightbulbs', completed: false },
-    { description: 'Kill Thistles', completed: true }
-  ];
+export class ToDoListComponent implements OnInit {
+  items: TodoListItem[];
 
+  constructor(private service: TodoDataService) { }
+
+  ngOnInit() {
+    this.items = this.service.getTodoList();
+  }
   markComplete(item: TodoListItem) {
     item.completed = true;
   }
-  total(what: HTMLInputElement) {
-    console.log(what.value);
-    this.items.unshift({ description: what.value, completed: false });
-    what.value = '';
+  add(what: string) {
+    this.items.unshift({ description: what, completed: false });
   }
   removeCompleted() {
     this.items = this.items.filter(item => item.completed === false);
   }
 }
+
